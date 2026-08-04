22524d6591e38aeb4165948ffa2b7d5ba6fbd27e
elvnshuti-codes <elvnshuti@gmail.com>
2026-08-04 11:00:59 +0300

    feat: add resource controller and routes for laptop registrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaptopRegistrationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('registrations', LaptopRegistrationController::class);
